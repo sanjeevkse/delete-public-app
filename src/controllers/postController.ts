be97c56c3d353643cd55821ca3667a0772b6a754
Sanjeev Shetty <sanjeevkse@gmail.com>
2025-10-30 14:43:50 +0530

    fix: temp

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -540,9 +540,13 @@ export const listPosts = asyncHandler(async (req: AuthenticatedRequest, res: Res
   const posts = rows.map((post) => formatPostForResponse(post, userId)).map((post) => {
     const w = 400; //  + Math.floor(Math.random() * 200);
     const h = 300; //  + Math.floor(Math.random() * 200);
-    return {...post, mediaUrl: `https://picsum.photos/seed/${post.id}-${Math.random()
+    const medias = (post.media as any)?.map((media: any) => ({
+      ...media,
+      mediaUrl: `https://picsum.photos/seed/${post.id}-${Math.random()
       .toString(36)
-      .substring(2, 8)}/${w}/${h}`};
+      .substring(2, 8)}/${w}/${h}`
+    }));
+    return {...post, media: medias };
   });
 
   return sendSuccessWithPagination(res, posts, pagination, "Posts retrieved successfully");
