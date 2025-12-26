07bbe42e790f058bc642df04b6e71b706793a1a6
Sanjeev Shetty <sanjeevkse@gmail.com>
2025-12-26 11:11:20 +0530

    d

---
+++ src/models/MetaBusinessCategory.ts
@@ -0,0 +1,83 @@
+import {
+  CreationOptional,
+  DataTypes,
+  InferAttributes,
+  InferCreationAttributes,
+  Model
+} from "sequelize";
+
+import sequelize from "../config/database";
+
+class MetaBusinessCategory extends Model<
+  InferAttributes<MetaBusinessCategory>,
+  InferCreationAttributes<MetaBusinessCategory>
+> {
+  declare id: CreationOptional<number>;
+  declare dispName: string;
+  declare description: CreationOptional<string | null>;
+  declare icon: CreationOptional<string | null>;
+  declare status: CreationOptional<number>;
+  declare createdBy: CreationOptional<number | null>;
+  declare updatedBy: CreationOptional<number | null>;
+  declare createdAt: CreationOptional<Date>;
+  declare updatedAt: CreationOptional<Date>;
+}
+
+MetaBusinessCategory.init(
+  {
+    id: {
+      type: DataTypes.BIGINT.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    dispName: {
+      field: "disp_name",
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true
+    },
+    description: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    icon: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+      comment: "Icon URL or base64 encoded icon"
+    },
+    status: {
+      type: DataTypes.TINYINT,
+      allowNull: false,
+      defaultValue: 1,
+      comment: "1=active, 0=inactive"
+    },
+    createdBy: {
+      field: "created_by",
+      type: DataTypes.BIGINT.UNSIGNED,
+      allowNull: true
+    },
+    updatedBy: {
+      field: "updated_by",
+      type: DataTypes.BIGINT.UNSIGNED,
+      allowNull: true
+    },
+    createdAt: {
+      field: "created_at",
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    updatedAt: {
+      field: "updated_at",
+      type: DataTypes.DATE,
+      allowNull: false
+    }
+  },
+  {
+    sequelize,
+    tableName: "tbl_meta_business_category",
+    timestamps: true,
+    underscored: true
+  }
+);
+
+export default MetaBusinessCategory;
